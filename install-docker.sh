45f3bf04f7cda0ede4aa9b39f4132b662c3843ec
Elio Tohme <eliotohme@gmail.com>
2017-07-21 11:35:10 +0300

    bug fix

--- install-docker.sh
+++ install-docker.sh
@@ -19,13 +19,13 @@ sudo add-apt-repository \
 
 sudo apt-get update
 
-sudo apt-get -y install docker-engine
+sudo apt-get -y -f install docker-engine
 
 sudo apt-get -y -f install python3
 sudo apt-get -y -f install libreadline-dev libncurses5-dev libssl1.0.0 tk8.5-dev zlib1g-dev liblzma-dev
 
-sudo apt-get -y -f install python3
+sudo apt-get -y -f install python3 python3-pip
 
-pip install docker-compose
+pip3 install docker-compose
 
 
