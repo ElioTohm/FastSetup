fbc77b9ae29b131191a94f9e338ea686f77740ee
Elio Tohme <eliotohme@gmail.com>
2017-07-21 11:25:39 +0300

    bug fix

--- install-docker.sh
+++ install-docker.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -f install curl \
+sudo apt-get -y -f install curl \
     linux-image-extra-$(uname -r) \
     linux-image-extra-virtual
 
-sudo apt-get install apt-transport-https \
+sudo apt-get -y install apt-transport-https \
                    ca-certificates 
 
 curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
 
 sudo apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
 
-  pub   4096R/2C52609D 2015-07-14
+pub   4096R/2C52609D 2015-07-14
         Key fingerprint = 5811 8E89 F3A9 1289 7C07  0ADB F762 2157 2C52 609D
-  uid                  Docker Release Tool (releasedocker) <docker@docker.com>
+uid                  Docker Release Tool (releasedocker) <docker@docker.com>
 
 sudo add-apt-repository \
        "deb https://apt.dockerproject.org/repo/ \
