5997bea44efd6bc76bf6126a069d83cd1c26ea58
elio <eliotohme@gmail.com>
2018-02-22 12:45:32 +0200

    refactoring code in install-docker rename kuadm install

--- install-docker.sh
+++ install-docker.sh
@@ -3,10 +3,9 @@
 sudo apt-get update
 sudo apt-get -y -f install curl \
     linux-image-extra-$(uname -r) \
-    linux-image-extra-virtual
-
-sudo apt-get -y install apt-transport-https \
-                   ca-certificates 
+    linux-image-extra-virtual \
+    apt-transport-https \
+    ca-certificates 
 
 curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
 
@@ -19,12 +18,7 @@ sudo add-apt-repository \
 
 sudo apt-get update
 
-sudo apt-get -y -f install docker-engine
-
-sudo apt-get -y -f install python3
-sudo apt-get -y -f install libreadline-dev libncurses5-dev libssl1.0.0 tk8.5-dev zlib1g-dev liblzma-dev
-
-sudo apt-get -y -f install python3 python3-pip
+sudo apt-get -y -f install python3 python3-pip docker-engine libreadline-dev libncurses5-dev libssl1.0.0 tk8.5-dev zlib1g-dev liblzma-dev
 
 sudo pip3 install docker-compose
 
