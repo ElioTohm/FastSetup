dcad5953cb0fc3b37ed7560c910a2db76e05724b
elio <eliotohme@gmail.com>
2018-04-28 14:50:45 +0300

    update

--- ffmpegbuild.sh
+++ ffmpegbuild.sh
@@ -1,5 +1,37 @@
 git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg/
-sudo apt-get install build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev
+sudo apt-get install build-essential \ 
+yasm \
+cmake \
+libtool \
+libc6 \
+libc6-dev \
+unzip \
+wget \
+libnuma \
+libnuma-dev \
+libfdk-aac-dev \
+libmp3lame-dev \
+libopus-dev \
+autoconf \
+automake \
+build-essential \
+libass-dev \
+libfreetype6-dev \
+libsdl2-dev \
+libtheora-dev \
+libva-dev \
+libvdpau-dev \
+libvorbis-dev \
+libxcb1-dev \
+libxcb-shm0-dev \
+libxcb-xfixes0-dev \
+mercurial \
+pkg-config \
+texinfo \
+zlib1g-dev \
+libx264-dev
+
+libvpx-dev 
 cd ffmpeg && \
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
 --prefix="$HOME/ffmpeg_build" \
@@ -18,13 +50,7 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
 --enable-libvorbis \
 --enable-libvpx \
 --enable-libx264 \
---enable-nonfree \
---enable-nvenc \
---enable-cuda \
---enable-cuvid \
---enable-libnpp \
---extra-cflags=-I/usr/local/cuda-9.1/include \
---extra-ldflags=-L/usr/local/cuda-9.1/lib64 && \
+--enable-nonfree 
 PATH="$HOME/bin:$PATH" make && \
 make install && \
 hash -r
