72460b715666467f2dfe1d27e532f48a0f073be0
elio_tohme <eliotohme@gmail.com>
2018-11-01 08:30:25 +0200

    docker now installs universally using get endpoint from docker.com and added nvidia hardware accelerator to ffmpeg build

--- ffmpegbuild.sh
+++ ffmpegbuild.sh
@@ -1,5 +1,8 @@
+# add graphics driver repo
+add-apt-repository ppa:graphics-drivers/ppa
 # install dependencies
-sudo apt-get update -qq && sudo apt-get -y install \
+# install nvidia driver 410 and cuda tools to be able to use nvidia NVENC NVDEC in ffmpeg
+apt update -qq && apt-get -y install \
   autoconf \
   automake \
   build-essential \
@@ -18,8 +21,11 @@ sudo apt-get update -qq && sudo apt-get -y install \
   pkg-config \
   texinfo \
   wget \
-  zlib1g-dev
-
+  zlib1g-dev \
+  mercurial \
+  libnuma-dev \
+  nvidia-driver-410 \
+  cuda-10-0
 mkdir -p ~/ffmpeg_sources ~/bin
 
 ##########################################################################################
@@ -32,85 +38,96 @@ tar xjvf nasm-2.13.03.tar.bz2 && \
 cd nasm-2.13.03 && \
 ./autogen.sh && \
 PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" && \
-make && \
-make install
+make -j4 && \
+make -j4 install
 # yasm
 cd ~/ffmpeg_sources && \
 wget -O yasm-1.3.0.tar.gz https://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz && \
 tar xzvf yasm-1.3.0.tar.gz && \
 cd yasm-1.3.0 && \
 ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" && \
-make && \
-make install
+make -j4 && \
+make -j4 install
 # h264
 cd ~/ffmpeg_sources && \
 git -C x264 pull 2> /dev/null || git clone --depth 1 https://git.videolan.org/git/x264 && \
 cd x264 && \
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --enable-pic && \
-PATH="$HOME/bin:$PATH" make && \
-make install
+PATH="$HOME/bin:$PATH" make -j4 && \
+make -j4 install
+
 # h265
-sudo apt-get install mercurial libnuma-dev && \
 cd ~/ffmpeg_sources && \
 if cd x265 2> /dev/null; then hg pull && hg update; else hg clone https://bitbucket.org/multicoreware/x265; fi && \
 cd x265/build/linux && \
 PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED=off ../../source && \
-PATH="$HOME/bin:$PATH" make && \
-make install
+PATH="$HOME/bin:$PATH" make -j4 && \
+make -j4 install
 # vp9
 cd ~/ffmpeg_sources && \
 git -C libvpx pull 2> /dev/null || git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git && \
 cd libvpx && \
 PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm && \
-PATH="$HOME/bin:$PATH" make && \
-make install
+PATH="$HOME/bin:$PATH" make -j4 && \
+make -j4 install
 # aac
 cd ~/ffmpeg_sources && \
 git -C fdk-aac pull 2> /dev/null || git clone --depth 1 https://github.com/mstorsjo/fdk-aac && \
 cd fdk-aac && \
 autoreconf -fiv && \
 ./configure --prefix="$HOME/ffmpeg_build" --disable-shared && \
-make && \
-make install
+make -j4 && \
+make -j4 install
 # lame
 cd ~/ffmpeg_sources && \
 wget -O lame-3.100.tar.gz https://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz && \
 tar xzvf lame-3.100.tar.gz && \
 cd lame-3.100 && \
 PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm && \
-PATH="$HOME/bin:$PATH" make && \
-make install
+PATH="$HOME/bin:$PATH" make -j4 && \
+make -j4 install
 # opus
 cd ~/ffmpeg_sources && \
 git -C opus pull 2> /dev/null || git clone --depth 1 https://github.com/xiph/opus.git && \
 cd opus && \
 ./autogen.sh && \
 ./configure --prefix="$HOME/ffmpeg_build" --disable-shared && \
-make && \
-make install
+make -j4 && \
+make -j4 install
 # av1
 cd ~/ffmpeg_sources && \
 git -C aom pull 2> /dev/null || git clone --depth 1 https://aomedia.googlesource.com/aom && \
 mkdir aom_build && \
 cd aom_build && \
 PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED=off -DENABLE_NASM=on ../aom && \
-PATH="$HOME/bin:$PATH" make && \
-make install
+PATH="$HOME/bin:$PATH" make -j4 && \
+make -j4 install
 # ffmpeg snapshot
 cd ~/ffmpeg_sources && \
 wget -O ffmpeg-snapshot.tar.bz2 https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2 && \
 tar xjvf ffmpeg-snapshot.tar.bz2 && \
-cd ffmpeg && \
 
+cd ~/ffmpeg_sources && \
+git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git && \
+make -j4  && \
+make -j4 install
+
+cd ~/ffmpeg_sources/ffmpeg && \
 # add path and set config and build ffmpeg
+# flags for cuda where added to be able to use libnpp
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
   --prefix="$HOME/ffmpeg_build" \
   --pkg-config-flags="--static" \
   --extra-cflags="-I$HOME/ffmpeg_build/include" \
+  --extra-cflags="-I/usr/local/cuda/include/" \
+  --extra-ldflags="-L/usr/local/cuda/lib64/" \
   --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
   --extra-libs="-lpthread -lm" \
   --bindir="$HOME/bin" \
   --enable-gpl \
+  --enable-cuda \
+  --enable-cuvid \
+  --enable-nvenc \
   --enable-libaom \
   --enable-libass \
   --enable-libfdk-aac \
@@ -121,9 +138,10 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --enable-libvpx \
   --enable-libx264 \
   --enable-libx265 \
+  --enable-libnpp \
   --enable-nonfree && \
-PATH="$HOME/bin:$PATH" make && \
-make install && \
+PATH="$HOME/bin:$PATH" make -j4 && \
+make -j4 install && \
 hash -r
 
 source ~/.profile
